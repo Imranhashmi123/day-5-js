bf927d140780b318004309d0dc86d6c41cd79d08
imran <imranejazhashmi@gmail.com>
2024-01-06 16:34:14 +0530

    day 5 js

---
+++ script.js
@@ -0,0 +1,216 @@
+// //1.
+// const arr = Array();
+// console.log(arr);
+
+// //2.
+
+// var array = [1,2,3,4,5];
+// //3.
+
+// var lnght = array.length;
+// console.log(lnght)
+// //4.
+// console.log(array[0]);
+// console.log(array[3]);
+// console.log(array[4]);
+
+// //5.
+// var mixedDataTypes = [1,"apple",100,10.1,true];
+// console.log(mixedDataTypes.length);
+// //6.
+// var itCompanies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'];
+// //7.
+// console.log(itCompanies);
+// //8.
+// console.log(itCompanies.length);
+// //9.
+
+//10.
+// console.log(itCompanies[0]);
+// console.log(itCompanies[1]);
+// console.log(itCompanies[2]);
+// console.log(itCompanies[3]);
+// console.log(itCompanies[4]);
+// //11.
+// let result1 = itCompanies[0].toUpperCase();
+// console.log(result1);
+// let result2 = itCompanies[1].toUpperCase();
+// console.log(result2);
+// let result3 = itCompanies[2].toUpperCase();
+// console.log(result3);
+// let result4 = itCompanies[3].toUpperCase();
+// console.log(result4);
+// let result5 = itCompanies[4].toUpperCase();
+// console.log(result5);
+//12.
+// let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon, oracle and Amazon are big IT companies';
+// const companies = companiesString.split(',')
+// console.log(companies);
+//13.
+
+// comapnyName = prompt("Enter comapny name");
+// if( comapnyName == "Facebook"){
+//     console.log("Facebook");
+// }
+// else if(comapnyName == "Google"){
+//     console.log("Google");
+// }
+// else if(comapnyName == "Microsoft"){
+//     console.log("Microsoft");
+// }
+// else if(comapnyName == "Apple"){
+//     console.log("Apple")
+// }
+// else if(comapnyName == "IBM"){
+//     console.log("IBM")
+// }
+// else if(comapnyName == "Oracle"){
+//     console.log("Oracle")
+// }
+// else if(comapnyName == "Amazon"){
+//     console.log("Amazon")
+// }
+// else{
+//     console.log("company is not found")
+// }
+
+
+//14.
+
+
+
+// const companies = ["Facebook",
+// "Google",
+//  "Microsoft", 
+//  "Apple",
+//   "IBM",
+//    "Oracle", 
+//     "Amazon"];
+
+// const filtered = [];
+// for(let comapny of companies){
+//     let count =0;
+//     for (let char of comapny ){
+//         if(char === 'o' ||  char === '0'){
+//             count++;
+//         }
+//     }
+//     if(count > 1){
+//         filtered.push(comapny)
+//     }
+// }
+// console.log(filtered)
+
+// 15
+
+// const companies = ["Facebook",
+// "Google",
+//  "Microsoft", 
+//  "Apple",
+//   "IBM",
+//    "Oracle", 
+//     "Amazon"];
+// console.log(companies.sort());
+
+// 16
+// const companies = ["Facebook",
+// "Google",
+//  "Microsoft", 
+//  "Apple",
+//   "IBM",
+//    "Oracle", 
+//     "Amazon"];
+// console.log(companies.reverse());
+
+// 17.
+
+// const companies = ["Facebook",
+// "Google",
+//  "Microsoft", 
+//  "Apple",
+//   "IBM",
+//    "Oracle", 
+//     "Amazon"];
+// console.log(companies.shift[2]);
+// 18.
+
+// const companies = ["Facebook",
+// "Google",
+//  "Microsoft", 
+//  "Apple",
+//   "IBM",
+//    "Oracle", 
+//     "Amazon"];
+// companies.splice(0,3)
+// console.log(companies);
+
+// 19.
+
+// const companies = ["Facebook",
+// "Google",
+//  "Microsoft", 
+//  "Apple",
+//   "IBM",
+//    "Oracle", 
+//     "Amazon"];
+// var companiess = companies.slice(0,-3)
+// console.log(companiess)
+
+
+// 20.
+// const companies = ["Facebook",
+// "Google",
+//  "Microsoft", 
+//  "Apple",
+//   "IBM",
+//    "Oracle", 
+//     "Amazon"];
+
+// companies.shift();
+// console.log(companies);
+
+
+// 21.
+// const companies = ["Facebook",
+// "Google",
+//  "Microsoft", 
+//  "Apple",
+//   "IBM",
+//    "Oracle", 
+//     "Amazon"];
+
+// companies.splice(1,2);
+// console.log(companies);
+
+// 22.
+
+// const companies = ["Facebook",
+// "Google",
+//  "Microsoft", 
+//  "Apple",
+//   "IBM",
+//    "Oracle", 
+//     "Amazon"];
+
+// companies.splice(-1);
+// console.log(companies);
+
+
+// 23.
+
+// const companies = ["Facebook",
+// "Google",
+//  "Microsoft", 
+//  "Apple",
+//   "IBM",
+//    "Oracle", 
+//     "Amazon"];
+
+// companies.splice();
+// console.log(companies);
+
+
+
+
+// level 2
+// 1.
